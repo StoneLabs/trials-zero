eebeb204c2e18a1457f4f147785cc38544f91d95
StoneLabs <levyehrstein@googlemail.com>
2018-03-14 06:03:19 +0100

    Dropped mobile support (for now)

--- Assets/Scripts/Scene/UIHandler.cs
+++ Assets/Scripts/Scene/UIHandler.cs
@@ -5,13 +5,10 @@ using UnityEngine.UI;
 
 public class UIHandler : MonoBehaviour
 {
-	public bool overrideWithKeyboardInput = true;
-    
     public SceneManager sceneManager;
 
     public Slider ui_throttle;
     public Slider ui_balance;
-    public Toggle ui_mobile;
 
 	public void Start()
 	{
@@ -22,15 +19,14 @@ public class UIHandler : MonoBehaviour
     
 	void Update()
 	{
-		if (overrideWithKeyboardInput = !ui_mobile.isOn)
-		{
-            if (Input.GetKeyDown("r"))
-                ResetBike();
-            sceneManager.bikeManager.SetThrust(Input.GetAxis("Vertical"));
-            ui_throttle.value = Input.GetAxis("Vertical");
-			sceneManager.bikeManager.SetBalance(Input.GetAxis("Horizontal"));
-            ui_balance.value = Input.GetAxis("Horizontal");
-		}
+        if (Input.GetKeyDown("r"))
+            ResetBike();
+
+        sceneManager.bikeManager.SetThrust(Input.GetAxis("Vertical"));
+        sceneManager.bikeManager.SetBalance(Input.GetAxis("Horizontal"));
+        
+        ui_throttle.value = Input.GetAxis("Vertical");
+        ui_balance.value = Input.GetAxis("Horizontal");
 	}
 
     public void ResetBike()
@@ -38,18 +34,5 @@ public class UIHandler : MonoBehaviour
         ui_throttle.value = 0;
         ui_balance.value = 0;
         sceneManager.Respawn();
-    }
-
-    public void ThrottleChanged()
-    {
-        sceneManager.bikeManager.SetThrust(ui_throttle.value);
-    }
-    public void BalanceChanged()
-    {
-        sceneManager.bikeManager.SetBalance(ui_balance.value);
-    }
-    public void ResetClicked()
-    {
-        ResetBike();
     }
 }
