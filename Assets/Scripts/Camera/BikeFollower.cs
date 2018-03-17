3aba9bb7976ed44588cadf27d5e03d864f0622d4
StoneLabs <levyehrstein@googlemail.com>
2018-03-17 20:51:56 +0100

    Changed camera position and follower script

--- Assets/Scripts/Camera/BikeFollower.cs
+++ Assets/Scripts/Camera/BikeFollower.cs
@@ -5,10 +5,15 @@ using UnityEngine;
 /* Simple script to allow the camera to follow the bike through x and y axis.
  * Just a simple temp. solution. Needs furthor work.
  */
-public class FollowX : MonoBehaviour 
+public class BikeFollower : MonoBehaviour 
 {
 	//Target bike
 	public SceneManager sceneManager;
+
+	[Header("Offset")]
+	public float distanceX;
+	public float distanceY;
+	public float distanceZ;
 
 	public void Start()
 	{
@@ -19,9 +24,9 @@ public class FollowX : MonoBehaviour
 	
 	void Update () 
 	{
-		// Follow bike on x and y axis. Z is set through editor placement.
-		this.transform.position = new Vector3(sceneManager.bikeManager.body.transform.position.x,
-											  sceneManager.bikeManager.body.transform.position.y + 1.5f,
-											  this.transform.position.z);
+		// Follow bike on all axis.
+		this.transform.position = new Vector3(sceneManager.bikeManager.body.transform.position.x + distanceX,
+											  sceneManager.bikeManager.body.transform.position.y + distanceY,
+											  sceneManager.bikeManager.body.transform.position.z + distanceZ);
 	}
 }
