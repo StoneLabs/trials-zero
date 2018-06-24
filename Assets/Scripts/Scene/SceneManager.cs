2f6764734eb826220740921d187222fd9781f5b3
StoneLabs <levyehrstein@googlemail.com>
2018-06-24 17:54:00 +0200

    Implemented statistics manager therefor time and respawn tracking

--- Assets/Scripts/Scene/SceneManager.cs
+++ Assets/Scripts/Scene/SceneManager.cs
@@ -6,16 +6,35 @@ using UnityEngine;
  */
 public class SceneManager : MonoBehaviour 
 {
-	// A prefab of the bike
-	public Transform prefab;
+	[Header("Bike related")]
 
+	// A prefab of the bike
+	[LabelOverride("Bike Prefab")]
+	public Transform prefab;
 	// References to the bike and the current spawnpoint set by editor or a CheckpointManager
+	[LabelOverride("Bike instance")]
 	public Transform bike = null;
+	[LabelOverride("Bike Manager")]
 	public BikeManager bikeManager = null;
+
+	[Header("Game logic related")]
+
+	[LabelOverride("Spawn Point")]
 	public Transform spawnPoint;
+	[LabelOverride("Statistics Manager")]
+	public StatisticsManager statsticsManager;
+	// Wether the player can currently respawn
+	private bool canRespawn = true;
+
+	void Start()
+	{
+		statsticsManager.HandleStart();
+	}
 
 	public void Respawn()
 	{
+		if (!canRespawn) return; 
+
 		// Create a new bike at the spawn point looking in positive x direction. 
 		Transform newBike = Instantiate(prefab, spawnPoint.transform.position, spawnPoint.transform.rotation, this.transform) as Transform;
 		Transform oldBike = bike;
@@ -26,5 +45,15 @@ public class SceneManager : MonoBehaviour
 
 		// Destroy the old bike after the references are set to prevent nullpointer exceptions
 		Destroy(oldBike.gameObject);
+
+		statsticsManager.HandleRespawn();
+	}
+
+	public void Finish()
+	{
+		Debug.Log("FINISHED");
+		canRespawn = false;
+		bikeManager.DisableDriver();
+		statsticsManager.HandleFinish();
 	}
 }
